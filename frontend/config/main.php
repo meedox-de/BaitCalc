94ac3210e78f8188b025f998a946777ba8d7cc67
Martin Thrömer <63214013+meedox-de@users.noreply.github.com>
2024-12-08 20:51:56 +0100

    save

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -7,7 +7,7 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-frontend',
+    'id' => 'bait-calc',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
@@ -22,7 +22,7 @@ return [
         ],
         'session' => [
             // this is the name of the session cookie used for login on the frontend
-            'name' => 'advanced-frontend',
+            'name' => 'bait-calc',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
